f4f415c239163d53ca50e8238227f2fb4a0112d1
santii-ssj <santino.escobar.pucheta@gmail.com>
2026-05-20 16:54:55 -0300

    Update pythonloskekeroscalculadora.py

--- pythonloskekeroscalculadora.py
+++ pythonloskekeroscalculadora.py
@@ -1,10 +1,31 @@
-def suma(a, b):
-    print("Realizando suma")
-    return a + b
 
-def resta(a, b):
-    print("Realizando resta")
-    return a - b
 
-print("Suma:", suma(10, 5))
-print("Resta:", resta(10, 5))
+def sumar(a, b):
+    resultado = a + b
+    print("El resultado de la suma es:", resultado)
+
+
+def restar(a, b):
+    resultado = a - b
+    print("El resultado de la resta es:", resultado)
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+
+print("1. Sumar")
+print("2. Restar")
+
+opcion = input("Elige una opción (1 o 2): ")
+
+
+if opcion == "1":
+    sumar(num1, num2)
+
+elif opcion == "2":
+    restar(num1, num2)
+
+else:
+    print("Opción inválida")
+
+
