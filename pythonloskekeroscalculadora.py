aa5f1dfe3c2024cd82ba2ac2a2ffeb81d1bcb1fb
santii-ssj <santino.escobar.pucheta@gmail.com>
2026-05-20 16:50:27 -0300

    Add files via upload

---
+++ pythonloskekeroscalculadora.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    print("Realizando suma")
+    return a + b
+
+def resta(a, b):
+    print("Realizando resta")
+    return a - b
+
+print("Suma:", suma(10, 5))
+print("Resta:", resta(10, 5))
